eabf5d60505385892a7fcb8cca5605138978c5aa
Rob Tirrell <r.tirrell@gmail.com>
2011-01-23 01:15:24 -0800

    Fix #/%23.

--- app/views.py
+++ app/views.py
@@ -1,16 +1,23 @@
 from django import http, shortcuts
 from django.utils import simplejson
+import cgi
 import re
 import twitter
 
 
 api = twitter.Api()
-tag_re = re.compile(r'(#[0-9A-Za-z_]+)')
+tag_re = re.compile(r'(#([0-9A-Za-z_]+))')
+user_re = re.compile(r'(@[0-9A-Za-z_]+)')
 
 def main(request):
   return shortcuts.render_to_response('scrapr/main.html', {})
 
 def search(request):
+  def format_tweet(tweet):
+    tweet = tag_re.sub(r'<a href="http://twitter.com/#!/search/%23\2">\1</a>', tweet)
+    tweet = user_re.sub(r'<a href="http://twitter.com/\1">\1</a>', tweet)
+    return tweet
+  
   q = request.POST.get('q')
   if q is None:
     return http.HttpResponseBadRequest()
@@ -20,7 +27,12 @@ def search(request):
   tags = {}
   
   for result in results:
-    response_data['results'].append(result.AsDict())
+    result_dict = result.AsDict()
+    print result_dict
+    result_dict['formatted_tweet'] = format_tweet(result_dict['text'])
+    print result_dict
+    
+    response_data['results'].append(result_dict)
     
     for tag_match in tag_re.finditer(result.text):
       name = tag_match.groups(1)[0]
