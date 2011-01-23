49e23674e8ac7cf424ebbc03a746d177f9e75cd6
Rob Tirrell <r.tirrell@gmail.com>
2011-01-22 18:28:21 -0800

    Remove useless pylint warnings.

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import * #@UnusedWildImport
 from scrapr.app import views as scrapr_views
 
 # Uncomment the next two lines to enable the admin:
